ea0929c090509636c9e293ba6ab5b53b41133e4b
KNXKO <lukaslechovic@gmail.com>
2024-04-03 15:47:52 +0200

    Final

--- cookieClicker.py
+++ cookieClicker.py
@@ -1,34 +1,61 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# Presmerovanie na stránku
-driver.get("https://google.com")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie_id = "bigCookie"
+count_ofcookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
+
+# Odkliknutie prvotného okna ak sa zobrazí
+try:
+    input_element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "fc-primary-button"))
+    )
+    input_element.click()
+except NoSuchElementException:
+    print("Prvotné okno nebolo nájdené.")
 
 # Pocka kym najde element a odklikne
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, "L2AGLb"))
-)
-input_element = driver.find_element(By.ID, "L2AGLb")
-input_element.click()
+try:
+    setLang = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='langSelect-EN']"))
+    )
+    setLang.click()
+except:
+    print("Element s id='langSelect-EN' nebol nájdený.")
 
-# Napisanie do search baru "lukas lechovic"
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("lukas lechovic" + Keys.ENTER)
+time.sleep(3)
 
-# Pocka kym sa vyhladá text a klikne
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Lukáš Lechovič KNXKO"))
-)
-link = driver.find_element(By.PARTIAL_LINK_TEXT, "Lukáš Lechovič KNXKO")
-link.click()
+cookie = driver.find_element(By.ID, cookie_id)
 
-time.sleep(7)
-driver.quit()
+while True:
+    try:
+        cookie.click()
+    except StaleElementReferenceException:
+        cookie = driver.find_element(By.ID, cookie_id)
+        cookie.click()
+
+    count_ofcookies = driver.find_element(By.ID, count_ofcookies_id).text.split(" ")[0]
+    count_ofcookies = int(count_ofcookies.replace(",",""))
+
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price)
+
+        if count_ofcookies >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
